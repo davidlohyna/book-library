173f86a94ac9cae1996d4b8ff7960c161863765d
David Lohyna <david.lohyna@gmail.com>
2023-07-21 20:00:27 +0200

    Fixes form

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, redirect, render_template, request, url_for
-from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, SubmitField
-from wtforms.validators import URL, DataRequired
+
+# from flask_wtf import FlaskForm
+# from wtforms import SelectField, StringField, SubmitField
+# from wtforms.validators import URL, DataRequired
 
 app = Flask(__name__)
 
@@ -10,13 +11,17 @@ all_books = []
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"],
+        }
+        all_books.append(new_book)
+        return redirect(url_for("home"))
     return render_template("add.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
