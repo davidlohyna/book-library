17a10b3af05d118dd40d206b750cd1adcb823de6
David Lohyna <david.lohyna@gmail.com>
2023-07-22 17:01:33 +0200

    adds sqlalchemy

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-import sqlite3
-
 from flask import Flask, redirect, render_template, request, url_for
-
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE,\
-          author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
-)
-
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
-# from flask_wtf import FlaskForm
-# from wtforms import SelectField, StringField, SubmitField
-# from wtforms.validators import URL, DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+# Optional: But it will silence the deprecation warning in the console.
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 all_books = []
+
+
+# CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f"<Book {self.title}>"
 
 
 @app.route("/")
@@ -36,3 +37,7 @@ def add():
         all_books.append(new_book)
         return redirect(url_for("home"))
     return render_template("add.html")
+
+
+with app.app_context():
+    db.create_all()
