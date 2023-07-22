0c821f412b5865bad65d4e0c919ea380c5328007
David Lohyna <david.lohyna@gmail.com>
2023-07-22 14:43:55 +0200

    adds database to the library

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import sqlite3
+
 from flask import Flask, redirect, render_template, request, url_for
 
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE,\
+          author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
+)
+
+cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+db.commit()
 # from flask_wtf import FlaskForm
 # from wtforms import SelectField, StringField, SubmitField
 # from wtforms.validators import URL, DataRequired
